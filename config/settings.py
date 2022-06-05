134b75cbaf45407f2a41352ff88526291f28caec
Aitor Lería <aitorleria@gmail.com>
2022-06-06 01:07:54 +0200

    Sites revert

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     # REST Framework
     'rest_framework.authtoken',
     'rest_framework',
